efd9bdbe440af3cf65927acdd32f484dc39f5bd5
kendlera <test@test.com>
2018-12-08 09:33:57 -0500

    2018 day 8

---
+++ 2018day8.jl
@@ -0,0 +1,89 @@
+function readInput(filename::String)
+	fd = open(filename, "r")
+	input = read(fd, String)
+	return input
+end 
+
+function parseInput(puzzInput::String)
+	inputs = split(puzzInput)
+	data = map(x->parse(Int64,x), inputs)
+	return data
+end
+
+mutable struct node
+	num_children::Int64
+	num_metadata::Int64
+	children::Array
+	metadata::Array
+	nodeValue::Int64
+end
+
+
+function constructTree(data, expected)
+	if length(data) == 0
+		return node(0, 0, [], []), 0
+	end
+	# pattern is num_children, num_medatdata, children, metadata
+	done = false
+	x = 1 # indexer
+	nodesChildren = []
+	while !done
+		item = node(data[x], data[x+1], [], [], 0)
+		x += 2
+		if item.num_children > 0
+			nodeChildren, numConsumed = constructTree(data[x:end], item.num_children)
+			item.children = nodeChildren
+			x += numConsumed
+		end
+		# println(data)
+		# println("Evalutating ", item, " new index at ", x)
+		item.metadata = data[x:x+(item.num_metadata-1)]
+		# print(x, " plus ", item.num_metadata, ": ")
+		x += item.num_metadata 
+		# println(x)
+		# println(item)
+		push!(nodesChildren, item)
+		done = (expected == length(nodesChildren))
+	end
+	# println("Returning ", nodesChildren, " and consumed ", x)
+	return nodesChildren, x-1
+end
+
+function sumMetadata(tree)
+	total = 0
+	for elem in tree 
+		if length(elem.metadata) > 0
+			total += sum(elem.metadata)
+		end
+		for child in elem.children
+			total += sumMetadata(child)
+		end
+	end
+	return total
+end
+
+function calculateValues(tree)
+	for child in tree.children
+		calculateValues(child)
+	end
+	if length(tree.children) == 0 
+		if length(tree.metadata) > 0
+			tree.nodeValue = sum(tree.metadata)
+		end
+	else 
+		total = 0 
+		# println(tree.children)
+		for item in tree.metadata 
+			if item > length(tree.children)
+				continue
+			end
+			total += tree.children[item].nodeValue 
+		end
+		tree.nodeValue = total
+	end
+	return tree
+end
+
+# println(constructTree(parseInput(readInput(filename)), 1)[1][1])
+# println(calculateValues(constructTree(parseInput(readInput(filename)), 1)[1][1]))
+
